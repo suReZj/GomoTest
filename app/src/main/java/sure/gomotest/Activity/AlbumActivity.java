e3ebb6bccaf715905d0d28c71889e56167027732
suReZj <1014652997@qq.com>
2018-03-08 09:26:29 +0800

    解决上拉分页加载

---
+++ app/src/main/java/sure/gomotest/Activity/AlbumActivity.java
@@ -0,0 +1,15 @@
+package sure.gomotest.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sure.gomotest.R;
+
+public class AlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album);
+    }
+}
