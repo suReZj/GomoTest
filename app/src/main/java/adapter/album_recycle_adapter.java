8799c7b88c87ab5013056a59f12869be361d4498
zhangzijian <1014652997@qq.com>
2018-03-12 16:32:44 +0800

    修改minSdkVersion

--- app/src/main/java/adapter/album_recycle_adapter.java
+++ app/src/main/java/adapter/album_recycle_adapter.java
@@ -2,6 +2,8 @@ package adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,6 +17,7 @@ import java.util.List;
 
 import bean.MediaBean;
 import sure.gomotest.R;
+import util.imageCache;
 
 /**
  * Created by zhangzijian on 2018/03/12.
@@ -23,6 +26,7 @@ import sure.gomotest.R;
 public class album_recycle_adapter extends RecyclerView.Adapter<album_recycle_adapter.ViewHolder> {
     private Context context;
     private List<MediaBean> list;
+    private imageCache imageCache;
 
     static class ViewHolder extends RecyclerView.ViewHolder{
         private ImageView imageView;
@@ -40,6 +44,7 @@ public class album_recycle_adapter extends RecyclerView.Adapter<album_recycle_ad
 
     public album_recycle_adapter(List<MediaBean> list) {
         this.list = list;
+        this.imageCache = new imageCache(context);
     }
 
     @Override
@@ -55,6 +60,24 @@ public class album_recycle_adapter extends RecyclerView.Adapter<album_recycle_ad
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         Glide.with(context).load(list.get(position).getLocalPath()).into(holder.imageView);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(list.get(position).getLocalPath(), options);
+
+        int inSampleSize = 1;
+        if (options.outWidth > holder.imageView.getWidth() || options.outHeight > holder.imageView.getHeight()) {
+            int widthRatio = Math.round((float) options.outWidth / (float) holder.imageView.getWidth());
+            int heightRatio = Math.round((float) options.outHeight / (float) holder.imageView.getHeight());
+            inSampleSize = Math.min(widthRatio, heightRatio);
+        }
+        options.inSampleSize = inSampleSize;
+
+        options.inJustDecodeBounds = false;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+
+//        Bitmap bitmap = BitmapFactory.decodeFile(file_path, options);
+
         if (mOnItemClickLitener != null) {
             holder.imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
--- app/src/main/java/sure/gomotest/MyApplication.java
+++ app/src/main/java/sure/gomotest/MyApplication.java
@@ -5,7 +5,6 @@ import android.content.Context;
 
 import com.previewlibrary.ZoomMediaLoader;
 
-import util.imageLoader;
 
 /**
  * Created by zhangzijian on 2018/03/08.
@@ -18,7 +17,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
-        ZoomMediaLoader.getInstance().init(new imageLoader());
     }
 
     public static Context getInstance() {
